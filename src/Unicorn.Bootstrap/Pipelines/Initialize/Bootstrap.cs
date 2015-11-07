059fb91d52d6b61f1f707c6d8a26ead858f10904
Mark Cassidy <markc@corepoint-it.net>
2015-11-07 13:50:36 +0100

    Basic setup

---
+++ src/Unicorn.Bootstrap/Pipelines/Initialize/Bootstrap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Unicorn.Configuration;
+using Unicorn.ControlPanel;
+using Unicorn.Logging;
+using Unicorn.Predicates;
+
+namespace Unicorn.Bootstrap.Pipelines.Initialize
+{
+    public class Bootstrap
+    {
+        public void Process(object args)
+        {
+            var configNames = GetConfigNames();
+
+            var bootstrapConfigurations = GetBootstrapConfigurations(configNames);
+
+            SyncBootstrapConfigurations(bootstrapConfigurations);
+        }
+
+        public virtual void SyncBootstrapConfigurations(IConfiguration[] bootstrapConfigurations)
+        {
+            foreach (var configuration in bootstrapConfigurations)
+            {
+                var logger = configuration.Resolve<ILogger>();
+                var helper = configuration.Resolve<SerializationHelper>();
+
+                try
+                {
+                    logger.Info(string.Empty);
+                    logger.Info(configuration.Name + " is being synced. (Unicorn.Bootstrap)");
+
+                    // Need to check with @kamsar on this. Leaving this code in, forces a reference to sitecore.kernel
+                    //using (new TransparentSyncDisabler())
+                    //{
+                    var pathResolver = configuration.Resolve<PredicateRootPathResolver>();
+
+                    var roots = pathResolver.GetRootSerializedItems();
+
+                    helper.SyncTree(configuration, null, roots);
+                    //}
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex);
+                    break;
+                }
+            }
+        }
+
+        public virtual IConfiguration[] GetBootstrapConfigurations(string[] configNames)
+        {
+            return ControlPanelUtility.ResolveConfigurationsFromQueryParameter(string.Join("^", configNames));
+        }
+
+        public virtual string[] GetConfigNames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
